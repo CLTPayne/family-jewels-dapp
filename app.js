9186cd37dc9e86e415598411ca95a30400070785
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-09-04 16:49:52 +0100

    Add port variable
    * Give flexibility for deployment or alternative port number

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv').config();
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => console.log('App is listening on port 3000!'))
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(3000, () => console.log(`Server is listening on port ${PORT}!`));
