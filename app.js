e0393e0162bb1de2e6d8a60123d16ea8b7d07e58
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-09-05 14:37:20 +0100

    Add basic HTML page
    * Ensure server set up can render HTML

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/index.html')));
 
 app.listen(3000, () => console.log(`Server is listening on port ${PORT}!`));
